16b58a8bd9f35c282536008f636e4cbde7c9f53a
Hashim <hashimabufara3@gmail.com>
2026-08-18 23:56:43 +0300

    Fix root route redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost"],
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/ar",
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
